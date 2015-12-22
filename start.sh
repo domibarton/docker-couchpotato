93ed43808ab93bb18b598b56b0d5e4af37ae0b20
Dominique Barton <dbarton@confirm.ch>
2015-12-22 10:50:56 +0100

    BUGFIX: Updating git repo before checkout

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+echo "Updating CouchPotato git repository..."
+git pull
+
 if [[ -n "${VERSION}" ]]
 then
     echo "Checking out SABnzbd version '${VERSION}'..."
     git checkout ${VERSION}
 fi
-
-echo "Updating CouchPotato..."
-git pull
 
 CONFIG=${CONFIG:-/datadir/config.ini}
 echo "Starting CouchPotato with config '${CONFIG}'..."
